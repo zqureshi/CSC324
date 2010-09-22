35dceb394fdcc5f3feab9f106c1ef0085565b466
Zeeshan Qureshi <zeeshan@zqureshi.in>
2010-09-22 01:35:14 -0400

    started module TreeOperations

---
+++ a0/TreeOperations.py
@@ -0,0 +1,11 @@
+# CSC 324 Fall 2010 - Assignment 0
+#
+# Zeeshan Qureshi
+# zeeshan.qureshi@utoronto.ca
+
+# Import all List function
+from List import *
+from ListOperations import *
+
+# Import all Tree function
+from Tree import *
