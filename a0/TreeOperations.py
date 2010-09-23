f026b20a6cade10e4484905149f28cd5a4e24f65
Zeeshan Qureshi <zeeshan@zqureshi.in>
2010-09-22 20:01:35 -0400

    completed Q2:b nestedListsToTree

--- a0/TreeOperations.py
+++ a0/TreeOperations.py
@@ -18,6 +18,14 @@ def treeToNestedLists(t):
         cons(treeToNestedLists(t.left), 
           cons(treeToNestedLists(t.right), None)))
 
+def nestedListsToTree(l):
+  if (rest(l) == None):
+    return Tree(first(l), None, None)
+  else:
+    return Tree(first(l), 
+        nestedListsToTree(first(rest(l))),
+        nestedListsToTree(first(rest(rest(l)))))
+
 def treeToString(t):
   if(t.left == None and t.right == None):
     return t.label
