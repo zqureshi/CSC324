0206c3df7cd42f24f795feb1faa2b2ff80ac4e91
Zeeshan Qureshi <zeeshan@zqureshi.in>
2010-09-24 00:24:23 -0400

    made functions back into normal ones instead of classes with a 'call' method

--- a0/ListOperations.py
+++ a0/ListOperations.py
@@ -5,23 +5,21 @@
 
 from List import *
 
-class isSubsequence:
-  def call(self, l1, l2):
-    if (l1 == None):
-      return True
-    elif (l2 == None):
-      return False
-    elif (first(l1) == first(l2)):
-      return self.call(rest(l1), rest(l2))
-    else:
-      return self.call(l1, rest(l2))
+def isSubsequence(l1, l2):
+  if (l1 == None):
+    return True
+  elif (l2 == None):
+    return False
+  elif (first(l1) == first(l2)):
+    return isSubsequence(rest(l1), rest(l2))
+  else:
+    return isSubsequence(l1, rest(l2))
 
-class map:
-  def call(self, fn, l):
-    if (l == None):
-      return
-    else:
-      return cons(fn.call(first(l)), self.call(fn, rest(l)))
+def map(fn, l):
+  if (l == None):
+    return
+  else:
+    return cons(fn.call(first(l)), map(fn, rest(l)))
 
 class Conser:
   def __init__(self, obj):
@@ -30,20 +28,17 @@ class Conser:
   def call(self, l):
     return cons(self.obj, l)
 
-class consAll:
-  def call(self, obj, l):
-    return map().call(Conser(obj), l)
+def consAll( obj, l):
+    return map(Conser(obj), l)
 
-class append:
-  def call(self, l1, l2):
-    if (l1 == None):
-      return l2
-    else:
-      return cons(first(l1), self.call(rest(l1), l2))
+def append(l1, l2):
+  if (l1 == None):
+    return l2
+  else:
+    return cons(first(l1), append(rest(l1), l2))
 
-class subsequences:
-  def call(self, l):
-    if(l == None):
-      return cons(None, None)
-    else:
-      return append().call(self.call(rest(l)), consAll().call(first(l), self.call(rest(l))))
+def subsequences(l):
+  if(l == None):
+    return cons(None, None)
+  else:
+    return append(subsequences(rest(l)), consAll(first(l), subsequences(rest(l))))
