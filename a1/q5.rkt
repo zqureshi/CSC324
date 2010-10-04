fcc9e723dbed5824fc826ba388525e00e199a29d
Zeeshan Qureshi <zeeshan@zqureshi.in>
2010-10-04 03:05:58 -0400

    completed q5: length-tail

--- a1/q5.rkt
+++ a1/q5.rkt
@@ -6,3 +6,8 @@
       (let ([res (box (void))]) ; declare variable to store result of recursive call
         (length-primitive (rest l) res)
         (set-box! len (+ (unbox res) 1)))))
+
+(define (length-tail l len)
+  (unless (empty? l)
+    (set-box! len (+ (unbox len) 1))
+    (length-tail (rest l) len)))
