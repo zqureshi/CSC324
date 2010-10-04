c2afef8c768daf22dd62f6bd5ebb77701d6eee42
Zeeshan Qureshi <zeeshan@zqureshi.in>
2010-10-04 16:35:34 -0400

    completed q3: rearrangements

--- a1/q3.rkt
+++ a1/q3.rkt
@@ -7,3 +7,17 @@
               (map
                (lambda (x) (cons (first l) x))
                (subsequences (rest l))))))
+
+; append element e at all positions in suffix
+(define (insert-at-all e pref suff)
+  (if (empty? suff)
+      (list (append (append pref (list e))))
+      (append (list (append pref (list e) suff))
+              (insert-at-all e (append pref (list (first suff))) (rest suff)))))
+
+(define (rearrangements l)
+  (if (empty? l)
+      '(())
+      (apply append 
+             (map (lambda (x) (insert-at-all (first l) '() x))
+                  (rearrangements (rest l))))))
