9feabf636a9b0dedaea512df95338ddef51887da
Zeeshan Qureshi <zeeshan@zqureshi.in>
2010-10-04 00:13:44 -0400

    removed testing code from q2

--- a1/q2.rkt
+++ a1/q2.rkt
@@ -8,11 +8,3 @@
         [(p (List-first l))
          (List (f (List-first l)) (map-such-that (List-rest l) f p))]
         [else (map-such-that (List-rest l) f p)]))
-
-;; Prints out a List
-(define (List-print l)
-  (display "(")
-  (map-such-that l (lambda (x) (display " ") (display x)) (lambda (x) #t))
-  (display " )")
-  (newline))
-
