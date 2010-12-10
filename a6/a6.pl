111c0dd1b327b64784e10fd407bde79c50adcb35
Zeeshan Qureshi <zeeshan@zqureshi.in>
2010-12-09 20:06:49 -0500

    A6: completed q2

--- a6/a6.pl
+++ a6/a6.pl
@@ -1,2 +1,12 @@
+/* Question 1 */
 natFrom(S, N) :- N is (S + 1).
 natFrom(S, N) :- S1 is (S + 1), natFrom(S1, N).
+
+/* Question 2a */
+square(S) :- natFrom(0, N), S is (N * N).
+
+/* Question 2b */
+squareFrom(N, S) :- square(S), S >= N.
+
+/* Question 2c */
+firstSquareFrom(N, S) :- squareFrom(N, S), S >= N, !.
