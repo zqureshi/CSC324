52edf99ed73ede856377ae4d80feda1a22365ddb
Zeeshan Qureshi <zeeshan@zqureshi.in>
2010-12-09 21:05:57 -0500

    A6: completed q3a and b

--- a6/a6.pl
+++ a6/a6.pl
@@ -10,3 +10,17 @@ squareFrom(N, S) :- square(S), S >= N.
 
 /* Question 2c */
 firstSquareFrom(N, S) :- squareFrom(N, S), S >= N, !.
+
+/* Question 3a */
+sorted([]) :- !.
+sorted([_]) :- !.
+sorted([F,S | T]) :- S > F, sorted([S | T]).
+
+/* prime(P) */
+prime(P) :- P >= 2, noDivisorFrom(P,2).
+noDivisorFrom(P,P) :- !.
+noDivisorFrom(P,D) :- M is P mod D, M > 0, D1 is D + 1, noDivisorFrom(P, D1).
+
+/* Question 3b */
+firstPrime([H | _], P) :- prime(H), P is H, !.
+firstPrime([_ | T], P) :- firstPrime(T, P).
